3c7147bd6c714e078675ce7496e8875d6ade3c29
Shubham-Vandara <shubhvandara@gmail.com>
2024-03-18 14:17:33 +0530

    Update Design

--- JS/index.js
+++ JS/index.js
@@ -62,7 +62,6 @@ $(document).on("ready", function () {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          autoplay: false,
         },
       },
       // You can unslick at a given breakpoint now by adding:
@@ -75,11 +74,10 @@ $(document).on("ready", function () {
 // TYPED JS CODE
 var typed = new Typed("#auto-type", {
   strings: [
-    "Programmer",
-    "Web-Developer",
-    "Frontend-Developer",
-    "Web-Designer",
-    "SEO Executive",
+    "Website Development",
+    "Frontend Development",
+    "User-Friendly Website Design",
+    "Custom Website Design",
   ],
   typeSpeed: 100,
   backSpeed: 50,
@@ -87,3 +85,41 @@ var typed = new Typed("#auto-type", {
   loop: true,
   showCursor: true,
 });
+
+// SCROLL TOP FUNCTIONALITY
+
+// Select the scroll-to-top button element
+const scrollToTop = document.querySelector("#up-arrow");
+
+// Add an event listener for the scroll event on the window
+window.onscroll = function () {
+  // Call the scrollFunction when the user scrolls
+  scrollFunction();
+};
+
+// Hide the scroll-to-top button initially
+scrollToTop.style.display = "none";
+
+// Define the scrollFunction
+function scrollFunction() {
+  // If the user has scrolled down more than 50 pixels
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    // Display the scroll-to-top button
+    scrollToTop.style.display = "flex";
+    // Apply a smooth transition effect
+    scrollToTop.style.transition = "1s";
+  } else {
+    // If not scrolled enough, hide the button
+    scrollToTop.style.display = "none";
+  }
+}
+
+// Add a click event listener to the scroll-to-top button
+scrollToTop.addEventListener("click", topFunction);
+
+// Define the topFunction
+function topFunction() {
+  // Scroll the page back to the top
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
